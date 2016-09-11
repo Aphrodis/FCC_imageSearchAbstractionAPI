65002495e63447c8e8a52fb55e5c9d00dce7c8f3
Jeremy Hurley <hurleyjer@yahoo.com>
2016-09-11 15:57:59 -0400

    working app2

--- README.ml
+++ README.ml
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Image Search Abstraction API - Free Code Camp Basejump</title>
-    <link rel='stylesheet' href='/stylesheets/style.css' />
-  </head>
-  <body>
-    <h1>Image Search Abstraction API</h1>
-    <h2>Free Code Camp Basejump</h2>
-    <h2>Example Uses:</h2>
-    <h3>Search</h3>
-    <code><%= location %>api/searchterm</code>
-    <h3>Paginated Search (offset=1 will offset top 10 results)</h3>
-    <code><%= location %>api/searchterm?offset=1</code>
-    <h3>Get History (last 10 queries)</h3>
-    <code><%= location %>api/history</code>
-  </body>
-</html>
+Image Search Abstraction API
+
+Free Code Camp Basejump
+
+Example Uses:
+
+Search
+
+http://localhost:3000/api/searchterm
+Paginated Search (offset=1 will offset top 10 results)
+
+http://localhost:3000/api/searchterm?offset=1
+Get History (last 10 queries)
+
+http://localhost:3000/api/history
